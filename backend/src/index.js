80cd9b22ddcfaa60590fcb7db458f83f15015581
Michael <checomichael2@gmail.com>
2019-02-16 17:32:39 -0500

    fixed cors issue

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,17 @@
 const { GraphQLServer } = require('graphql-yoga');
 const { prisma } = require('./generated/prisma-client');
 const { resolvers } = require('./resolvers');
-
 const server = new GraphQLServer({
 	typeDefs: 'src/schema.graphql',
 	resolvers,
 	context: { prisma },
 });
 
-server.start(() => console.log('server running on http://localhost:4000'));
+server.start(
+	{
+		cors: {
+			origin: 'http://localhost:3000',
+		},
+	},
+	() => console.log('server running on http://localhost:4000')
+);
