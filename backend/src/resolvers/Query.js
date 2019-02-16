4d1374d82f64bdcc53121fce244d27f834bd095d
Michael <checomichael2@gmail.com>
2019-02-16 17:32:14 -0500

    resolver for querying single item

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -2,6 +2,9 @@ const Query = {
 	items: (parent, args, ctx, info) => {
 		return ctx.prisma.items();
 	},
+	item: (parent, { id }, ctx, info) => {
+		return ctx.prisma.item({ id });
+	},
 };
 
 module.exports = {
