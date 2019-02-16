05a405174a4fb1023284708edbcd91d1e448de00
Michael <checomichael2@gmail.com>
2019-02-16 17:31:34 -0500

    expose query to user on each request

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -4,6 +4,15 @@ import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
 import withApollo from '../lib/withApollo';
 
 class MyApp extends App {
+	static async getInitialProps({ Component, ctx }) {
+		let pageProps = {};
+		if (Component.getInitialProps) {
+			pageProps = await Component.getInitialProps(ctx);
+		}
+		// this exposes the query to the user
+		pageProps.query = ctx.query;
+		return { pageProps };
+	}
 	render() {
 		const { Component, pageProps, apollo } = this.props;
 
