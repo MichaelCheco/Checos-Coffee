b54988dcab39ee3fa0a0db387914f20872f6bae2
Michael <checomichael2@gmail.com>
2019-02-17 12:26:32 -0500

    added queryparams for pagination

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,6 +1,9 @@
 const Query = {
 	items: (parent, args, ctx, info) => {
-		return ctx.prisma.items();
+		return ctx.prisma.items({
+			skip: args.skip,
+			first: args.first,
+		});
 	},
 	item: (parent, { id }, ctx, info) => {
 		return ctx.prisma.item({ id });
