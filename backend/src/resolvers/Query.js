991f3329218a3dfc7dfcd882a9ee1ec65012e4e7
Michael <checomichael2@gmail.com>
2019-02-17 18:01:55 -0500

    added filtering/pagination

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,9 +1,29 @@
 const Query = {
-	items: (parent, args, ctx, info) => {
-		return ctx.prisma.items({
+	items: async (parent, args, ctx, info) => {
+		const where = args.filter
+			? {
+					OR: [
+						{ description_contains: args.filter },
+						{ title_contains: args.filter },
+					],
+			  }
+			: {};
+		const items = await ctx.prisma.items({
+			where,
 			skip: args.skip,
 			first: args.first,
 		});
+		const count = await ctx.prisma
+			.itemsConnection({
+				where,
+				skip: args.skip,
+			})
+			.aggregate()
+			.count();
+		return {
+			items,
+			count,
+		};
 	},
 	item: (parent, { id }, ctx, info) => {
 		return ctx.prisma.item({ id });
