c905e48ab7f26dc117cccd9dbb7c29a76adc4301
Michael <checomichael2@gmail.com>
2019-02-19 14:53:00 -0500

    Mutation-signup resolver

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { APP_SECRET } = require('../utils');
 const Mutation = {
 	createItem: (parent, args, ctx, info) => {
 		return ctx.prisma.createItem({
@@ -15,11 +18,28 @@ const Mutation = {
 	deleteItem: (parent, { id }, ctx, info) => {
 		return ctx.prisma.deleteItem({ id });
 	},
-	// signup: async (parent, args, ctx, info) => {
-	// 	args.email = args.email.toLowerCase()
-	// 	const password = await bcrypt.hash(args.password, 10)
-	// 	const user =
-	// }
+	signup: async (parent, args, ctx, info) => {
+		args.email = args.email.toLowerCase();
+		const password = await bcrypt.hash(args.password, 10);
+		const user = await ctx.prisma.createUser({
+			...args,
+			password,
+		});
+		const token = jwt.sign({ userId: user.id }, APP_SECRET);
+		return { token, user };
+	},
+	signin: async (parent, args, ctx, info) => {
+		const user = await ctx.prisma.user({ email: args.email });
+		if (!user) {
+			throw new Error('No User Found');
+		}
+		const validUser = await bcrypt.compare(args.password, user.password);
+		if (!validUser) {
+			throw new Error('Invalid Credentials');
+		}
+		const token = jwt.sign({ userId: user.id }, APP_SECRET);
+		return { token, user };
+	},
 };
 
 module.exports = {
