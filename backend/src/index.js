f665e514ddebc140d1af6098fbab370391f59f97
Michael <checomichael2@gmail.com>
2019-02-19 14:53:47 -0500

    added request to context

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,16 @@
 const { GraphQLServer } = require('graphql-yoga');
 const { prisma } = require('./generated/prisma-client');
+const { APP_SECRET } = require('./utils');
 const { resolvers } = require('./resolvers');
 const server = new GraphQLServer({
 	typeDefs: 'src/schema.graphql',
 	resolvers,
-	context: { prisma },
+	context: request => {
+		return {
+			...request,
+			prisma,
+		};
+	},
 });
 
 server.start(
